bfd86ea522d635ab56da7230fb5667a049984354
Samuel de Andrade <samuelvasc11@gmail.com>
2025-06-08 17:58:16 -0300

    Create FuncoesListagem.java

---
+++ Funcoes/FuncoesListagem.java
@@ -0,0 +1,38 @@
+ private static void listarMateriais() {
+        System.out.println("\n--- Todos os Materiais ---");
+        List<Material> materiais = biblioteca.listarTodos();
+        
+        if (materiais.isEmpty()) {
+            System.out.println("Nenhum material cadastrado.");
+        } else {
+            for (Material material : materiais) {
+                System.out.println(material);
+            }
+        }
+    }
+    
+    private static void listarLivros() {
+        System.out.println("\n--- Livros Cadastrados ---");
+        List<Livro> livros = biblioteca.listarLivros();
+        
+        if (livros.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado.");
+        } else {
+            for (Livro livro : livros) {
+                System.out.println(livro);
+            }
+        }
+    }
+    
+    private static void listarRevistas() {
+        System.out.println("\n--- Revistas Cadastradas ---");
+        List<Revista> revistas = biblioteca.listarRevistas();
+        
+        if (revistas.isEmpty()) {
+            System.out.println("Nenhuma revista cadastrada.");
+        } else {
+            for (Revista revista : revistas) {
+                System.out.println(revista);
+            }
+        }
+    }
